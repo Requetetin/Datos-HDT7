bb383a0f5ae63d9f40e28e77186532888bff3c29
Requetetin <martinamadog@gmail.com>
2020-03-21 17:53:08 -0600

    Programa funcional completo

--- src/Main.java
+++ src/Main.java
@@ -14,25 +14,61 @@ public class Main{
 		
 
 		try{
+			//A partir de aqui se ingresan los datos del archivo al arbol
 			String line = "";
 			Scanner reader = new Scanner(new File("diccionario.txt"));	
 			while(reader.hasNextLine()){
 				line = reader.nextLine();
 				String[] parts = line.split(", "); 	
 
-				tree.addValue(temp= new ComparableAssociation<String,String>(parts[0], parts[1]));
+				tree.addValue(temp= new ComparableAssociation<String,String>(parts[0].replace("(",""), parts[1].replace(")","")));
 				
 
 			}
+
+			//Se realiza el recorrido inOrder del arbol
+			ArrayList<ComparableAssociation<String,String>> ordered =  tree.inOrder(tree.getRoot());
+			System.out.println("A continuacion se mostrara el arbol en inOrder:\n\n");
+			for(int i=0; i<ordered.size(); i++){
+				System.out.println("("+ordered.get(i).getKey()+", "+ordered.get(i).getValue()+")");
+			}
+			System.out.println("\n\n");
+
+
+			try{
+				String translation = "";
+				Scanner translator = new Scanner(new File("traduccion.txt"));
+				String pretrans = translator.nextLine();
+				String[] wBw = pretrans.split(" ");
+
+				System.out.println("La traduccion es la siguiente\n\n");
+				System.out.println(pretrans+" -->");
+
+				ArrayList<String> temporal = new ArrayList<String>();
+				for(int i=0; i<wBw.length;i++){
+					temporal.add(wBw[i]);
+					for(int j=0;j<ordered.size();j++){
+						if(wBw[i].equalsIgnoreCase(ordered.get(j).getKey())){
+							temporal.set(i, "*"+ordered.get(j).getValue()+"*");
+
+						}
+					}
+					
+				}
+				for(int i=0; i< temporal.size();i++){
+					translation += temporal.get(i)+" ";
+				}
+				System.out.println(translation+"\n\n");
+			}catch(Exception e){
+				System.out.println("El archivo a traducir no existe");
+			}
+
+
 		}catch(Exception e){
 			System.out.println("El archivo 'diccionario.txt' no ha sido encontrado");
 		}
 
-		ArrayList<ComparableAssociation<String,String>> ordered =  tree.inOrder(tree.getRoot());
-
-		for(int i=0; i<ordered.size(); i++){
-			System.out.println(ordered.get(i).getKey()+", "+ordered.get(i).getValue());
-		}
+		
 		
 
 	}
